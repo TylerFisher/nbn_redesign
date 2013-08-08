31eb54ce8abf47ae576c940b54dfe8a507ade6c1
Tyler Fisher <tyler@tylerjfisher.com>
2013-08-07 21:56:33 -0400

    adding a grab of the current site

---
+++ js/recent_comments_widget.js
@@ -0,0 +1,79 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 width: 32px;\
+	 height: 32px;\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_S69U4BPzPB/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/6341/8276/avatar92.jpg?1374380101"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_S69U4BPzPB/">CMags</a>\
+	 <span class="dsq-widget-comment"><p>WOO RAH</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://northbynorthwestern.com/story/dorms-of-years-past-willard-residential-college/">Dorms of years past: Willard Residential College | North by Northwestern</a>&nbsp;&middot;&nbsp;<a href="http://northbynorthwestern.com/story/dorms-of-years-past-willard-residential-college/#comment-971036017">2 weeks ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_H0mybwfNlb/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/6320/6923/avatar92.jpg?1374259317"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_H0mybwfNlb/">Seej</a>\
+	 <span class="dsq-widget-comment"><p>WOO SHACK</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://northbynorthwestern.com/story/dorms-of-years-past-willard-residential-college/">Dorms of years past: Willard Residential College | North by Northwestern</a>&nbsp;&middot;&nbsp;<a href="http://northbynorthwestern.com/story/dorms-of-years-past-willard-residential-college/#comment-969373027">2 weeks ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_NQ4tXqRCmK/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/6106/8603/avatar92.jpg?1373451382"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_NQ4tXqRCmK/">leo</a>\
+	 <span class="dsq-widget-comment"><p>some guys are just grossed out by the idea im not i enjoy having sex with my girl friend while...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://northbynorthwestern.com/story/sex-on-your-period/">Why you should still have sex on your period | North by Northwestern</a>&nbsp;&middot;&nbsp;<a href="http://northbynorthwestern.com/story/sex-on-your-period/#comment-954188209">1 month ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/dannjoroge/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/6048/8873/avatar92.jpg?1372871414"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/dannjoroge/">Dan Njoroge</a>\
+	 <span class="dsq-widget-comment"><p>this is quite interesting,how folks are making it big while still young..I\'m currently taking...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://northbynorthwestern.com/story/how-to-run-a-hedge-fund-from-your-dorm-room/">How to run a hedge fund from your dorm room | North by Northwestern</a>&nbsp;&middot;&nbsp;<a href="http://northbynorthwestern.com/story/how-to-run-a-hedge-fund-from-your-dorm-room/#comment-950748142">1 month ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_m9RvGPMzqL/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/4894/4612/avatar92.jpg?1374356019"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_m9RvGPMzqL/">Askani</a>\
+	 <span class="dsq-widget-comment"><p>Why is a coke a 1.25? Why are condoms 12.00? Why is gas 4.00? Why are paper towels 6.00? Why is...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://northbynorthwestern.com/story/are-there-really-3700-abortions-each-day-in-the-us/">Are there really 3,700 abortions each day in the U.S.? | North by Northwestern</a>&nbsp;&middot;&nbsp;<a href="http://northbynorthwestern.com/story/are-there-really-3700-abortions-each-day-in-the-us/#comment-949582081">1 month ago</a></p>\
+	 </li>\
+	 </ul>\
+');
